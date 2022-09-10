d7bc3cf2ede4da4069a393b289fca100e5ba376b
Dan Herbert <dyndns-namecom@hrbrt.co>
2022-09-10 09:42:08 -0700

    Lots of code cleanup
    - Stop passing config & state around as params to everything.
    - Don't use params for things that are easily gotten from the config
      file.
    - General naming improvements in some stuff

--- dynip.py
+++ dynip.py
@@ -25,11 +25,35 @@ SCRIPT_PATH=os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH=os.path.join(SCRIPT_PATH, 'config.yaml')
 STATE_PATH=os.path.join(SCRIPT_PATH, 'state.yaml')
 
+def get_config():
+    """Gets the config object from disk"""
+    with open(CONFIG_PATH, mode='rt', encoding="utf-8") as file_handle:
+        return yaml.safe_load(file_handle)
 
-def last_email_older_than(state, minutes):
+
+def get_state():
+    """Gets the state object from disk"""
+    if not exists(STATE_PATH):
+        state=BLANK_STATE
+    else:
+        with open(STATE_PATH, encoding="utf-8") as file_handle:
+            state=yaml.safe_load(file_handle)
+        if state is None or not state:
+            state=BLANK_STATE
+    return state
+
+
+def write_state(state):
+    """Writes state object to disk"""
+    with open(STATE_PATH, mode='wt', encoding='utf-8') as state_file:
+        yaml.dump(state, state_file)
+
+
+def last_email_older_than(minutes):
     """Check the state yaml to see if the last email sent happened longer than
        <minutes> ago from now."""
     # Just an arbitrary datetime in the past as a fallback for missing value.
+    state=get_state()
     last_notified=datetime(2022, 1, 1, 0, 0, 0, 0)
     lastmail_str = state['last_fatal_mail_sent']
     if lastmail_str is not None and len(lastmail_str) > 0:
@@ -43,8 +67,9 @@ def log(val):
     print(datetime.now().isoformat() + ' ' + val)
 
 
-def send_mail(config, mail_subject, mail_body):
+def send_mail(subject, body):
     """Send an email using settings from config.yaml"""
+    config=get_config()
     ctx = ssl.create_default_context()
     host = config['mail_host']
     port = config['mail_port']
@@ -56,15 +81,17 @@ def send_mail(config, mail_subject, mail_body):
         msg = f"""\
 From: {from_addr}
 To: {to_addr}
-Subject: {mail_subject}
+Subject: {subject}
 
-{mail_body}"""
+{body}"""
         server.login(config['mail_user'], config['mail_pass'])
         server.sendmail(from_addr, to_addr, msg)
 
 
-def abort_on_failure(config, state, label, resp):
+def abort_on_failure(label, resp):
     """Abort this script completely if the response is non 200 status."""
+    config=get_config()
+    state=get_state()
     if resp.status_code != 200:
         log('FATAL HTTP ERROR...')
         log('Non-200 status code from ' + label + ' api.')
@@ -72,7 +99,7 @@ def abort_on_failure(config, state, label, resp):
         log('status_code: ' + str(resp.status_code))
         log('elapsed: ' + str(resp.elapsed))
         log('resp body: ' + resp.text)
-        if config['send_emails'] and last_email_older_than(state, 120):
+        if config['send_emails'] and last_email_older_than(120):
             mail_subj = (config['domain_being_updated'] +
                     ' Dynamic DNS Fatal API Call For ' + label + ' API')
             mail_body = f"""
@@ -83,24 +110,25 @@ status_code: {str(resp.status_code)}
 elapsed: {str(resp.elapsed)}
 resp body: {resp.text}
 """
-            send_mail(config, mail_subj, mail_body)
+            send_mail(mail_subj, mail_body)
             state['last_fatal_mail_sent'] = datetime.now().isoformat()
-            with open(STATE_PATH, mode='wt', encoding='utf-8') as state_file:
-                yaml.dump(state, state_file)
+            write_state(state)
 
-        if config['send_emails'] and not last_email_older_than(state, 120):
+        if config['send_emails'] and not last_email_older_than(120):
             log('Last email sent less than 2 hours ago. Intentionally not sending another')
         sys.exit(0)
 
 
-def timeout_abort(config, state, label, url):
+def timeout_abort(label, url):
     """Abort this script and log it as a timeout error."""
+    config=get_config()
+    state=get_state()
     reqs_timeout=config['requests_timeout_seconds']
     log('FATAL TIMEOUT ERROR...')
     log('Timeout from ' + label + ' api.')
     log('url: ' + url)
     log('timeout seconds: ' + str(reqs_timeout))
-    if config['send_emails'] and last_email_older_than(state, 120):
+    if config['send_emails'] and last_email_older_than(120):
         subj = config['domain_being_updated'] + ' Dynamic DNS Fatal API Call For ' + label + ' API'
         body = f"""
 FATAL TIMEOUT ERROR
@@ -109,18 +137,20 @@ url: {url}
 timeout_seconds: {reqs_timeout}
 retries: {config['getreq_retry_limit']}
 """
-        send_mail(config, subj, body)
+        send_mail(subj, body)
         state['last_fatal_mail_sent'] = datetime.now().isoformat()
-        with open(STATE_PATH, mode='wt', encoding='utf-8') as file:
-            yaml.dump(state, file)
+        write_state(state)
 
-    if config['send_emails'] and not last_email_older_than(state, 120):
+    if config['send_emails'] and not last_email_older_than(120):
         log('Last email sent less than 2 hours ago. Intentionally not sending another')
     sys.exit(0)
 
 
-def get_with_retry(config, state, url, label, timeout, auth=None):
+def get_with_retry(url, label, auth=None):
     """Gets the specified URL and retries if there's a timeout."""
+    config=get_config()
+    timeout=config['requests_timeout_seconds']
+    retry_limit=config['getreq_retry_limit']
     tries=0
     while True:
         try:
@@ -128,27 +158,26 @@ def get_with_retry(config, state, url, label, timeout, auth=None):
             break
         except requests.exceptions.ReadTimeout:
             tries+=1
-            if tries < config['getreq_retry_limit']:
+            if tries < retry_limit:
                 time.sleep(timeout)
                 continue
-            timeout_abort(config, state, label, url)
+            timeout_abort(label, url)
         except requests.exceptions.ConnectionError:
             tries+=1
-            if tries < config['getreq_retry_limit']:
+            if tries < retry_limit:
                 time.sleep(timeout)
                 continue
-            timeout_abort(config, state, label, url)
+            timeout_abort(label, url)
 
-    abort_on_failure(config, state, label, resp)
+    abort_on_failure(label, resp)
     return resp
 
 
-def get_wan_ip(config, state, old_wan_ip, reqs_timeout):
+def get_wan_ip(old_wan_ip, reqs_timeout):
     """Gets the current wan IP."""
-    wan_ip=get_with_retry(config,
-                          state,
-                          config['wanip_endpoint'],
-                          'wan_ip_endpoint'
+    config=get_config()
+    wan_ip=get_with_retry(config['wanip_endpoint'],
+                          'wan_ip_endpoint',
                           reqs_timeout).text
 
     if wan_ip == old_wan_ip:
@@ -178,18 +207,15 @@ def main():
     old_wan_ip=state['wan_ip']
     reqs_timeout=config['requests_timeout_seconds']
 
-    wan_ip=get_wan_ip(config, state, old_wan_ip, reqs_timeout)
+    wan_ip=get_wan_ip(old_wan_ip, reqs_timeout)
 
     record_api_url=(f"https://{config['api_host']}/v4/domains/"
                     f"{config['domain_name']}/records/"
                     f"{str(config['domain_id'])}")
     auth_params=(config['username'], config['token'])
-    get_resp=get_with_retry(config,
-                            state,
-                            record_api_url,
-                            'record_list_api'
-                            auth=auth_params,
-                            timeout=reqs_timeout)
+    get_resp=get_with_retry(record_api_url,
+                            'record_list_api',
+                            auth=auth_params)
 
     existing_record=get_resp.json()
     existing_record['answer']=wan_ip
@@ -205,8 +231,10 @@ def main():
                 timeout=reqs_timeout
             )
     except requests.exceptions.ReadTimeout:
-        timeout_abort(config, state, 'UPDATERECORD', record_api_url)
-    abort_on_failure(config, state, 'UPDATERECORD', put_resp)
+        timeout_abort('UPDATERECORD', record_api_url)
+    except requests.exceptions.ConnectionError:
+        timeout_abort('UPDATERECORD', record_api_url)
+    abort_on_failure('UPDATERECORD', put_resp)
 
     state['wan_ip']=wan_ip
     state['last_updated']=datetime.now().isoformat()
@@ -215,10 +243,9 @@ def main():
     if config['send_emails']:
         updated_ip_subj = config['domain_being_updated'] + ' IP Address has changed'
         updated_ip_body = f'Old IP: {old_wan_ip}\nNew IP: {wan_ip}'
-        send_mail(config, updated_ip_subj, updated_ip_body)
+        send_mail(updated_ip_subj, updated_ip_body)
 
-    with open(STATE_PATH, mode='wt', encoding='utf-8') as final_state_file:
-        yaml.dump(state, final_state_file)
+    write_state(state)
 
 
 if __name__ == '__main__':
